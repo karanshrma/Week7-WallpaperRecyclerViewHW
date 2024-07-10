e8a2829bbfa1f1f3e9e0935421b5f45ac64a99ad
Karan Sharma <ks2275863@gmail.com>
2024-07-10 17:53:11 +0530

    Singleton keyword

--- app/src/main/java/com/example/wallpaperapp/di/AppModule.kt
+++ app/src/main/java/com/example/wallpaperapp/di/AppModule.kt
@@ -4,9 +4,11 @@ import com.example.wallpaperapp.Utils.Constants.BASE_URL
 import com.example.wallpaperapp.data.api.PicSumApi
 import dagger.Provides
 import retrofit2.Retrofit
+import javax.inject.Singleton
 
 class AppModule {
 
+    @Singleton
     @Provides
     fun provideRetrofitApi(
         // Potential dependencies of this type
